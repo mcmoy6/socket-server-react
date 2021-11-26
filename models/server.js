ec8eb81211854f47af39fe817fa16e06cac627c5
Moisés <mcmoy6@gmail.com>
2021-11-25 21:28:57 -0600

    CORS habilitado

--- models/server.js
+++ models/server.js
@@ -3,7 +3,8 @@ const express  = require('express');
 const http     = require('http');
 const socketio = require('socket.io');
 const path     = require('path');
-const Sockets = require('./sockets');
+const Sockets  = require('./sockets');
+const cors     = require('cors');
 
 class Server {
 
@@ -16,12 +17,18 @@ class Server {
         this.server = http.createServer( this.app );
 
         // Configuración del socket server
-        this.io = socketio( this.server, { /* Configuraciones */ } );
+        this.io = socketio( this.server, { cors: {
+            origin: "*",
+            methods: ["GET", "POST", "PUT", "DELETE"]
+          } } );
     }
 
     middlewares() {
         // Desplegar el directorio de la carpeta publica
         this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
+
+        // Habilitar CORS
+        this.app.use( cors() );
     }
 
     configurarSockets() {
